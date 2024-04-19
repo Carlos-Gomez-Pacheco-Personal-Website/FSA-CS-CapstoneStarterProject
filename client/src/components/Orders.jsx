6f88373528f98a58f6890f231841cd38478e52ff
Carlos Gomez <carlosegomez001@hotmail.com>
2024-04-18 20:13:29 -0400

    style for title of website and logo, style for display orders

--- client/src/components/Orders.jsx
+++ client/src/components/Orders.jsx
@@ -1,16 +1,46 @@
 import PropTypes from "prop-types";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Orders = ({ orders }) => {
   return (
-    <div className="orders">
-      <h2>Orders</h2>
-      {orders.map((order) => (
-        <div key={order.id}>
-          <p>Order ID: {order.id}</p>
-          {/* <p>Total: {order.total}</p> */}
-        </div>
-      ))}
-    </div>
+    <Container>
+      <div
+        style={{
+          boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+          padding: "10px",
+          marginBottom: "20px",
+          borderRadius: "15px",
+          marginTop: "20px",
+          paddingBottom: "2px",
+        }}
+      >
+        <p className="center">Orders</p>
+      </div>
+      {orders.length > 0 ? (
+        <Row>
+          {orders.map((order, index) => (
+            <Col
+              md={12}
+              key={order.id}
+              style={{
+                borderBottom: "1px solid #ccc",
+                paddingTop: "10px",
+                boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+                borderRadius: "15px",
+                marginBottom: "8px",
+              }}
+            >
+              <p>
+                N°{index + 1} Order: {order.id}
+              </p>
+              {/* <p>Total: {order.total}</p> */}
+            </Col>
+          ))}
+        </Row>
+      ) : (
+        <p>No orders at this moment.</p>
+      )}
+    </Container>
   );
 };
 
